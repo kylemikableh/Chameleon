ee855d546028c3b2efd0b4006401476bcf05adfd
Kyle Mikolajczyk <kjmikolajczyk@wpi.edu>
2020-12-02 15:31:51 -0500

    Dispatch function and assigned keys on the piano to solenoids

--- Uno_Solenoids/solenoids/solenoids.ino
+++ Uno_Solenoids/solenoids/solenoids.ino
@@ -10,6 +10,13 @@
 #define SOLENOID_4_PIN 11
 #define SOLENOID_5_PIN 12
 #define SOLENOID_6_PIN 13
+
+#define MIDI_1 72
+#define MIDI_2 73
+#define MIDI_3 74
+#define MIDI_4 75
+#define MIDI_5 76
+#define MIDI_6 77
  
 void setup() 
 {
@@ -23,8 +30,6 @@ void setup()
   pinMode(SOLENOID_4_PIN, OUTPUT);
   pinMode(SOLENOID_5_PIN, OUTPUT);
   pinMode(SOLENOID_6_PIN, OUTPUT);
-
-  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop()
@@ -34,30 +39,75 @@ void loop()
   {
     // read the incoming byte:
     int inByte1 = Serial.read(); // read the first byte
+    delay(10); 
+    int inByte2 = Serial.read();
     Serial.print("RECIEVED ");
-    Serial.println(inByte1); // print the variable inByte1 to the serial monitor
-    if(inByte1 == 1)
-    {
-      digitalWrite(LED_BUILTIN, HIGH);
-      delay(500);
-      digitalWrite(LED_BUILTIN, LOW);
-    }
-    //we will also have to be reading the duration(note lenghts here)
+    Serial.print(inByte1); // print the variable inByte1 to the serial monitor
+    Serial.print(" ");
+    Serial.println(inByte2);
 
-    //Here we will have to assign the serial output variables to solenoids and dispatch
+    //Solenoid 1 will be C4 on the piano which is 72 from MIDI
+    dispatch(inByte1, inByte2); //Send notes recieved
   }
-//  else
-//  {
-//    //Did not recieve serial, panic!  
-//    Serial.println("Not recieving serial signal. Will retry in 5 seconds...");
-//    delay(5000);
-//  }
 }
 
 /**
- * I am thinking this function should actually send commands to solenoids
+ * This function handles the recieved channels and velocity from MIDI
+ * @args int channel MIDI channel recieved, int velocity Velocity recieved
+ * @return
  */
-void dispatch(int solenoid, int duration)
+void dispatch(int channel, int velocity)
 {
+  int pin = 0;
+  if(channel == MIDI_1) 
+  {
+    pin = SOLENOID_1_PIN;
+  }
+  else if(channel == MIDI_2) 
+  {
+    pin = SOLENOID_2_PIN;
+  }
+  else if(channel == MIDI_3) 
+  {
+    pin = SOLENOID_3_PIN;
+  }
+  else if(channel == MIDI_4) 
+  {
+    pin = SOLENOID_4_PIN;
+  }
+  else if(channel == MIDI_5) 
+  {
+    pin = SOLENOID_5_PIN;
+  }
+  else if(channel == MIDI_6) 
+  {
+    pin = SOLENOID_6_PIN;
+  }
+
+  if(pin == 0)
+  {
+    //Not a recognized note, return
+    return;
+  }
   
+  //0 velocity means stop note, aka set to low
+  if(velocity == 0)
+  {
+    digitalWrite(pin, LOW);
+    Serial.print("Set pin ");
+    Serial.print(pin);
+    Serial.println(" to LOW.");
+  }
+  else if(velocity > 0) //Means play, we do not have a velocity for a plectrum
+  {
+    digitalWrite(pin, HIGH);
+    Serial.print("Set pin ");
+    Serial.print(pin);
+    Serial.println(" to HIGH.");
+  }
+  else
+  {
+    //garbage recieved don't do anything
+    return;
+  }
 }
